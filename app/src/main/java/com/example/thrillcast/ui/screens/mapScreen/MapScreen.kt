72574c46c5e01fe12684ad64e982e8cb7b4ae4f8
Erik Aasen Eskedal <erik.eskedal@hotmail.com>
2023-03-28 19:13:51 +0200

    Midlertidig search bar

--- app/src/main/java/com/example/thrillcast/ui/screens/mapScreen/MapScreen.kt
+++ app/src/main/java/com/example/thrillcast/ui/screens/mapScreen/MapScreen.kt
@@ -1,25 +1,23 @@
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.viewinterop.AndroidView
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapView
 
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.google.maps.android.compose.*
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.google.android.gms.maps.model.*
-
 import com.example.thrillcast.R
+import com.google.android.gms.maps.model.*
+import com.google.maps.android.compose.*
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -34,7 +32,7 @@ fun MapScreen(viewModel: MapViewModel = viewModel()) {
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
-        topBar = {  },
+        topBar = { SearchBarDemo() },
         content = { paddingValues ->
             Column(
                 modifier = Modifier
@@ -57,6 +55,21 @@ fun MapScreen(viewModel: MapViewModel = viewModel()) {
         },
         bottomBar = { NavBar() }
     )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBarDemo() {
+    val searchTerm = remember { mutableStateOf("") }
+
+    Row {
+        OutlinedTextField(
+            value = searchTerm.value,
+            onValueChange = { searchTerm.value = it },
+            label = { Text("Find takeoff locations") },
+            modifier = Modifier.weight(1f)
+        )
+    }
 }
 
 @Preview
