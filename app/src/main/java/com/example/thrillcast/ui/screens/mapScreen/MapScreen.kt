99d80ec157e1f850e8a2e9d77d953b83d160b5b4
Siri <siri.eik@hotmail.com>
2023-03-28 11:29:33 +0200

    satte map i scaffold m navbar

--- app/src/main/java/com/example/thrillcast/ui/screens/mapScreen/MapScreen.kt
+++ app/src/main/java/com/example/thrillcast/ui/screens/mapScreen/MapScreen.kt
@@ -1,3 +1,4 @@
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -6,7 +7,10 @@ import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.model.MarkerOptions
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,27 +20,35 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.maps.android.compose.*
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MapScreen() {
     val norway = LatLng(62.0, 10.0)
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(norway, 5.5f)
     }
-    Surface(
+    Scaffold(
         modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        GoogleMap(
-            modifier = Modifier.fillMaxSize(),
-            cameraPositionState = cameraPositionState
-        ) {
-            Marker(
-                state = MarkerState(LatLng(60.05388889, 10.32250000)),
-                title = "Sundvollen",
-
-            )
-        }
-    }
+        topBar = {  },
+        content = { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+            ) {
+                GoogleMap(
+                    modifier = Modifier.fillMaxSize(),
+                    cameraPositionState = cameraPositionState
+                ) {
+                    Marker(
+                        state = MarkerState(LatLng(60.05388889, 10.32250000)),
+                        title = "Sundvollen",
+                        )
+                }
+            }
+        },
+        bottomBar = { NavBar() }
+    )
 }
 
 @Preview
