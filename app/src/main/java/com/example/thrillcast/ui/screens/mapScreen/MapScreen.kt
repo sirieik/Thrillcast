36dce07fd4854ca5803066c088a17b29d8671454
Erik Aasen Eskedal <erik.eskedal@hotmail.com>
2023-03-29 15:36:16 +0200

    Mer funksjonalitet og symboler rundt searchBar/top bar

--- app/src/main/java/com/example/thrillcast/ui/screens/mapScreen/MapScreen.kt
+++ app/src/main/java/com/example/thrillcast/ui/screens/mapScreen/MapScreen.kt
@@ -1,36 +1,21 @@
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-
-
-
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
-
-import androidx.navigation.NavHostController
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
-
 import com.example.thrillcast.R
 import com.google.android.gms.maps.model.*
-
 import com.google.maps.android.compose.*
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -70,18 +55,44 @@ fun MapScreen(viewModel: MapViewModel = viewModel(),) {
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 fun SearchBarDemo() {
-    val searchTerm = remember { mutableStateOf("") }
+    val searchInput = remember { mutableStateOf("") }
+    val hideKeyboard = LocalFocusManager.current
 
-    Row {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconButton(
+            onClick = {
+            /*TODO - legge til nav bar der man går tilbake til start skjerm*/
+            }
+        ) {
+            Icon(
+                imageVector = Icons.Filled.ArrowBack,
+                contentDescription = "Go back"
+            )
+        }
         OutlinedTextField(
-            value = searchTerm.value,
-            onValueChange = { searchTerm.value = it },
+            value = searchInput.value,
+            onValueChange = { searchInput.value = it },
             label = { Text("Find takeoff locations") },
             modifier = Modifier.weight(1f)
         )
+        IconButton(
+            onClick = {
+                searchInput.value = ""
+                hideKeyboard.clearFocus()
+            }
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Close,
+                contentDescription = "Exit search"
+            )
+        }
     }
 }
 
