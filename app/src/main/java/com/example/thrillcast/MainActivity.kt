b88b16e9f060a6f95ec1b643f0f1c757b71f0aa6
kamillmh <kamillmh@uio.no>
2023-03-31 14:46:31 +0200

    Penere introscreen

--- app/src/main/java/com/example/thrillcast/MainActivity.kt
+++ app/src/main/java/com/example/thrillcast/MainActivity.kt
@@ -3,22 +3,43 @@ package com.example.thrillcast
 import MapScreen
 import NavBar
 import ThrillCastApp
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.example.thrillcast.ui.screens.introScreen.IntroScreen
 import com.example.thrillcast.ui.theme.ThrillCastTheme
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.insets.navigationBarsWithImePadding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
+import com.google.android.exoplayer2.ui.StyledPlayerView
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.maps.android.compose.rememberCameraPositionState
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,8 +51,102 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     ThrillCastApp()
+
+                    //IntroScreen(getVideoUri())
                 }
             }
         }
     }
+    private fun getVideoUri(): Uri {
+        val rawId: Int = resources.getIdentifier("clouds", "raw", packageName)
+        val videoUri = "android.resource://$packageName/$rawId"
+        return Uri.parse(videoUri)
+    }
 }
+
+/**
+private fun Context.buildExoPlayer(uri: Uri) =
+    ExoPlayer.Builder(this).build().apply {
+        setMediaItem(MediaItem.fromUri(uri))
+        repeatMode = Player.REPEAT_MODE_ALL
+        playWhenReady = true
+        prepare()
+    }
+
+private fun Context.buildPlayerView(exoPlayer: ExoPlayer) =
+    StyledPlayerView(this).apply {
+        player = exoPlayer
+        layoutParams = FrameLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        useController = false
+        resizeMode = AspectRatioFrameLayout.RESIZE_MODE_ZOOM
+    }
+
+
+@Composable
+fun IntroScreen(videoUri: Uri) {
+    val context = LocalContext.current
+    //val passwordFocusRequester = FocusRequester()
+    //val focusManager = LocalFocusManager.current
+    val exoPlayer = remember { context.buildExoPlayer(videoUri) }
+
+    DisposableEffect(
+        AndroidView(
+            factory = { it.buildPlayerView(exoPlayer) },
+            modifier = Modifier.fillMaxSize()
+        )
+    ) {
+        onDispose {
+            exoPlayer.release()
+        }
+    }
+
+    ProvideWindowInsets {
+
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(24.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp, alignment = Alignment.Top),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.paragliding),
+                null,
+                Modifier.size(80.dp),
+                tint = Color.Black
+            )
+            Text("ThrillCast")
+        }
+        Column(
+            Modifier
+                .navigationBarsWithImePadding()
+                .padding(24.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp, alignment = Alignment.Bottom),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+
+
+            Button(
+                onClick = {},
+                modifier = Modifier.fillMaxWidth(),
+                //leadingIcon = {Icon(imageVector = Icons.Default.Person, null)}
+            )
+            {
+                Text("Paragliding", Modifier.padding(vertical = 8.dp))
+            }
+            Button(onClick = {}, modifier = Modifier.fillMaxWidth()) {
+                Text("Skydiving", Modifier.padding(vertical = 8.dp))
+            }
+            Button(onClick = {}, modifier = Modifier.fillMaxWidth()) {
+                Text("Hanggliding", Modifier.padding(vertical = 8.dp))
+            }
+        }
+    }
+}
+
+**/
--- app/src/main/java/com/example/thrillcast/ui/theme/Type.kt
+++ app/src/main/java/com/example/thrillcast/ui/theme/Type.kt
@@ -2,33 +2,44 @@ package com.example.thrillcast.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.thrillcast.R
 
 // Set of Material typography styles to start with
+
+val Exo = FontFamily(
+    Font(R.font.exo_regular, FontWeight.Bold),
+    Font(R.font.exo_semibolditalic)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    /*h1 = TextStyle(
+        fontFamily = Exo,
+        fontWeight = FontWeight.Bold,
+        fontSize = 25.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = Exo,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+        fontSize = 20.sp
+    ),*/
+    // Other default text styles to override
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontFamily = Exo,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        //lineHeight = 28.sp,
+        //letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = Exo,
+        fontWeight = FontWeight.Normal,
+        fontSize = 60.sp,
+        //lineHeight = 16.sp,
+        //letterSpacing = 0.5.sp
     )
-    */
+
 )
