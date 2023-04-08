a1cc652295ee54be3227db12579d24c4276ad5b2
sofiehel <sofiehel@uio.no>
2023-04-08 16:59:47 +0200

    InfoSheet

--- app/src/main/java/com/example/thrillcast/MainActivity.kt
+++ app/src/main/java/com/example/thrillcast/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.example.thrillcast
 
-import MapScreen
-import NavBar
 import ThrillCastApp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -9,16 +7,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.thrillcast.ui.theme.ThrillCastTheme
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
-import com.google.maps.android.compose.rememberCameraPositionState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
