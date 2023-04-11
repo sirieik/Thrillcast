633066c7e1aa105ecb105ec8ccf2fb53221bbe01
Siri <siri.eik@hotmail.com>
2023-04-11 11:32:26 +0200

    la til navigate funksjon i introscreen og den starter der

--- app/src/main/java/com/example/thrillcast/ui/screens/mapScreen/MapBottomInfoSheet.kt
+++ app/src/main/java/com/example/thrillcast/ui/screens/mapScreen/MapBottomInfoSheet.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.android.gms.maps.model.Marker
 import kotlinx.coroutines.launch
@@ -100,7 +101,7 @@ fun MapBottomInfoSheet(modifier: Modifier = Modifier, selectedMarker: Marker?, b
             },
         sheetPeekHeight = 235.dp,
         sheetGesturesEnabled = true,
-        scaffoldState = rememberBottomSheetScaffoldState()
+        scaffoldState = rememberBottomSheetScaffoldState(),
     ) {
         // A closing button for the sheet
         Button(onClick = { onCloseSheet() }) {
@@ -108,5 +109,4 @@ fun MapBottomInfoSheet(modifier: Modifier = Modifier, selectedMarker: Marker?, b
         }
     }
 }
-
 
