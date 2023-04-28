4ffe4d4f46fcd382c19b4c9a0cdd0f6dcd3071a4
Erik Aasen Eskedal <erik.eskedal@hotmail.com>
2023-04-28 12:35:52 +0200

    ting

--- app/src/main/java/com/example/thrillcast/ui/screens/favoritesScreen/FavoritesScreen.kt
+++ app/src/main/java/com/example/thrillcast/ui/screens/favoritesScreen/FavoritesScreen.kt
@@ -1,18 +1,21 @@
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.navigation.NavHostController
-import com.example.thrillcast.R
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.thrillcast.ui.viemodels.weather.WeatherViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FavoritesScreen() {
+fun FavoritesScreen(addedFavorites: List<WeatherViewModel>, weatherViewModel: WeatherViewModel = viewModel()) {
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {  },
@@ -22,7 +25,12 @@ fun FavoritesScreen() {
                     .fillMaxSize()
                     .padding(paddingValues)
             ) {
-                Text(text = stringResource(id = R.string.favourites))
+                Text(text = "Favorites")
+                LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                    /*items(addedFavorites) {
+                        NowWeatherCard(viewModel = weatherViewModel)
+                    }*/
+                }
             }
         }
     )
---
+++ app/src/main/java/com/example/thrillcast/ui/viemodels/favorites/FavoriteViewModel.kt
@@ -0,0 +1,2 @@
+package com.example.thrillcast.ui.viemodels.favorites
+
