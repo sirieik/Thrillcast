18199f2c5a076bfe2f329c9d783b469529ca1581
Clara Asplund <clarasplund@gmail.com>
2023-05-08 23:26:56 +0200

    10 enhetstester

---
+++ app/src/test/java/com/example/thrillcast/HolfuyRepositoryTest.kt
@@ -0,0 +1,57 @@
+package com.example.thrillcast
+
+import com.example.thrillcast.data.datamodels.Wind
+import com.example.thrillcast.data.repositories.HolfuyRepository
+import com.example.thrillcast.ui.screens.mapScreen.MarkerIconResource
+import com.example.thrillcast.ui.viemodels.map.Takeoff
+import com.google.android.gms.maps.model.LatLng
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Test
+
+class HolfuyRepositoryTest {
+
+    /*
+      suspend fun fetchTakeoffs(): List<com.example.thrillcast.ui.viemodels.map.Takeoff> {
+     */
+    @Test
+    fun takeOffListIsNotEmpty(){
+        //Act
+        val fetchTakeoffsResult = runBlocking{
+            HolfuyRepository().fetchTakeoffs()
+        }
+        //Assert
+        Assert.assertTrue(fetchTakeoffsResult.isNotEmpty())
+    }
+    /*
+     suspend fun fetchHolfuyObjects(): List<com.example.thrillcast.data.datamodels.HolfuyObject>
+     */
+    @Test
+    //test fungerer ikke akkurat nå, pga Holfuy API fungerer ikke.
+    fun fetchHolfuyObjectsIsNotEmpty(){
+        //Act
+        val holfuyObjectResult = runBlocking {
+            HolfuyRepository().fetchHolfuyStationWeather(101)
+        }
+        //Assert
+        Assert.assertNotNull(holfuyObjectResult)
+    }
+
+    @Test
+    //tester grønnhjul er mellom 0 til 360 grader
+    fun testFetchTakeoffs(){
+        //Act
+        val fetchTakeoffsResult = runBlocking{
+            HolfuyRepository().fetchTakeoffs()
+        }
+        //Assert
+        fetchTakeoffsResult.forEach {
+            val greenStart = it.greenStart
+            val greenStop = it.greenStop
+
+            Assert.assertTrue(greenStart in 0..360)
+            print("STOP: $greenStop")
+            Assert.assertTrue(greenStop in 0..360)
+        }
+    }
+}
