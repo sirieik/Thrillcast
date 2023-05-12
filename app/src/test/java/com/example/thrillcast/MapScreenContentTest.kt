573884b33b159bb193139f754ebd1f0b5b11f2ef
Clara Asplund <clarasplund@gmail.com>
2023-05-12 23:41:19 +0200

    oppdatert enhetstest

--- app/src/test/java/com/example/thrillcast/MapScreenContentTest.kt
+++ app/src/test/java/com/example/thrillcast/MapScreenContentTest.kt
@@ -1,15 +1,13 @@
 package com.example.thrillcast
 
-import com.example.thrillcast.data.datamodels.Wind
-import com.example.thrillcast.ui.screens.mapScreen.MarkerIconResource
-import com.example.thrillcast.ui.viewmodels.map.Takeoff
-import com.google.android.gms.maps.model.LatLng
+import checkWindConditions
+import com.example.thrillcast.ui.common.WindCondition
 import org.junit.Test
 import org.junit.Assert.*
 
 class MapScreenContentTest {
 
-/*
+    /*test1
 fun MarkerIcon(wind: Wind, takeoff: Takeoff): BitmapDescriptor {
     return if (isDegreeBetween((wind.direction?: 0.0).toDouble(), takeoff.greenStart, takeoff.greenStop)) {
         BitmapDescriptorFactory.fromResource(R.drawable.greendot)
@@ -17,8 +15,41 @@ fun MarkerIcon(wind: Wind, takeoff: Takeoff): BitmapDescriptor {
         BitmapDescriptorFactory.fromResource(R.drawable.red_dot)
     }
 }
+//ny test
+fun checkWindConditions(windDirection: Double?, windSpeed: Double?, greenStart: Int, greenStop: Int): WindCondition {
+    return if (isDegreeBetween(windDirection ?: 0.0, greenStart, greenStop) && (windSpeed ?: 0.0) in 3.0..5.0) {
+        WindCondition.GOOD
+    } else if (isDegreeBetween(windDirection ?: 0.0, greenStart, greenStop) && (windSpeed ?: 0.0) in 2.0..6.0) {
+        WindCondition.OKAY
+    } else {
+        WindCondition.BAD
+    }
+}
  */
     //for å sjekke at funskjon finner riktig farge av marker
+    @Test
+    fun testCheckWindConditions() {
+        //Arrange
+        val greenStart = 40
+        val greenStop = 90
+
+        //Act
+        val goodWind = checkWindConditions(60.0, 5.0,  greenStart, greenStop )
+        val okayWind = checkWindConditions(70.0, 6.0, greenStart, greenStop)
+        val badWind = checkWindConditions(88.0, 8.0, greenStart, greenStop)
+        val badWind2 = checkWindConditions(120.0, 3.0, greenStart, greenStop)
+
+        //Assert
+        assertEquals(WindCondition.GOOD, goodWind)
+        assertEquals(WindCondition.OKAY, okayWind)
+        assertEquals(WindCondition.BAD, badWind)
+        assertEquals(WindCondition.BAD, badWind2)
+
+    }
+}
+
+    /*
+
     @Test
     fun testMarkerIconFarge() {
     //Arrange
@@ -45,3 +76,4 @@ fun MarkerIcon(wind: Wind, takeoff: Takeoff): BitmapDescriptor {
     assertEquals(R.drawable.red_dot, redResult)
     }
 }
+*/
